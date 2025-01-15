6b300431cd10c31873d44a56cbfbd92fccddd1ac
Manish K Reddy <mreddy10@hawk.iit.edu>
2025-01-15 14:42:48 -0600

    Changes to be committed: 	modified:   app/page.tsx 	modified:   app/projects/project-data.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,44 @@ export default function Page() {
           Let’s connect and explore how we can collaborate on projects that
           shape the future.
         </p>
+        <h2>Education</h2>
+        <ul>
+          <li>
+            <strong>Master of Science in Computer Science</strong> (Aug 2023 – May 2025) <br />
+            Illinois Institute of Technology, Chicago, IL <br />
+            Coursework: Trustworthy Machine Learning, Marketing Strategy, Advanced Database Organization, Computer Networks.
+          </li>
+          <li>
+            <strong>Bachelor of Engineering with Honors, Computer Science and Engineering</strong> (Aug 2019 - Jun 2023) <br />
+            B.N.M. Institute of Technology, Bangalore, Karnataka, India | CGPA: 8.53 / 10.0 | First Class with Distinction <br />
+            Coursework: Machine Learning, Deep Learning, Data Science, Human Computer Interaction, Software Engineering.
+          </li>
+        </ul>
+        <h2>Work Experience</h2>
+        <ul>
+          <li>
+            <strong>Genisys Venture Analyst</strong> (Dec 2024 – Present) <br />
+            Kaplan Institute, Illinois Tech, Chicago <br />
+            Conducted deep market and technology analyses within digital transformation trends, particularly in AI and computing,
+            delivering strategic insights to guide investment recommendations for TechForward funding. Synthesized technology
+            readiness and commercialization strategies into Investment Summary Reports, highlighting key opportunities and risks
+            for executive decision-making.
+          </li>
+          <li>
+            <strong>Founder, CEO</strong> (Dec 2023 – Sep 2024) <br />
+            H2.0 Resilience, Illinois Tech, Chicago <br />
+            Sourced and assessed market opportunities for an explainable AI flood risk tool, identifying strategic spaces in flood
+            mitigation technologies and generating interest from 5+ government agencies. Reduced flood evaluation time by 90% using
+            Transformer-based analytics to enhance data visualization and stakeholder insights.
+          </li>
+          <li>
+            <strong>Machine Learning Research Intern</strong> (Jan 2023 – Jul 2023) <br />
+            VIGA Entertainment, Bangalore, India <br />
+            Led the team on "Face Solver" research project using Google MediaPipe for real-time 3D facial animation in Unreal Engine.
+            Designed an intuitive UX interface to facilitate seamless control of the animation system, reducing troubleshooting
+            incidents by consolidating functions into one easy-to-navigate platform.
+          </li>
+        </ul>
         <p>
           Built and maintained by{" "}
           <a href={socialLinks.linkedin} target="_blank">
--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,24 +7,24 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Project One",
+    title: "Adversarial Attack on Local LLMs (LARF)",
+    year: 2024,
+    description:
+      "Developed the Local Adversarial Refinement Framework (LARF) to identify vulnerabilities in 16 local LLMs, including Lllama2, SmoLLM and Deepseek-V2, using models like GPT-4 and Llama-2 for attack and evaluation, improving safety in Generative AI.",
+    url: "https://github.com/kredd10/JailbreakingLLMs",
+  },
+  {
+    title: "Tracking Template Face Mesh",
     year: 2023,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "Designed and implemented a pipeline integrating Google MediaPipe with game engine functionalities, achieving sub-2-second latency from input capture to display for real-time, AI-driven 3D facial motion capture across multiple subjects.",
+    url: "https://drive.google.com/file/d/1vCJ0_mewhgfF8m9jApNxOuAsxBaK3wl7/view?usp=sharing",
   },
   {
-    title: "Project Two",
-    year: 2022,
+    title: "Literature Survey on Tracking Template Face Mesh Using Stereo Video",
+    year: 2023,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Three",
-    year: 2021,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "Researched and evaluated 2D and 3D facial motion capture techniques, identifying limitations and proposing solutions to enable real-time, accessible applications in public settings.",
+    url: "https://www.taylorfrancis.com/chapters/edit/10.1201/9781003363781-48/literature-survey-tracking-template-face-mesh-using-stereo-video-kavitha-jayaram-ameer-suhail-aneesh-acharya-manish-reddy-mohammed-raheel",
   },
 ];
